abd86c4d97dd823943ae7dcb97c3ae2988671138
Joanna K <joannakleinrm@gmail.com>
2023-03-19 17:04:53 +0100

    js

--- fetchCountries.js
+++ fetchCountries.js
@@ -1,1 +1,13 @@
-export fetchCountries(name)
+const fetchCountriesInput = document.querySelector("#search-box");
+
+export function fetchCountries(country) {
+    const countryName = `https://restcountries.com/v3.1/name/` + fetchCountriesInput.value.trim() + `?fields=name,capital,population,flags,languages`;
+    return fetch(`${countryName}`)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        }
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,38 @@
 import './css/styles.css';
-
+import { fetchCountries } from '../fetchCountries';
 import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+const fetchCountriesInput = document.querySelector("#search-box");
+const countryList = document.querySelector(".country-list");
+const countryInfo = document.querySelector(".country-info");
+
+fetchCountriesInput.addEventListener("input", () => {
+    fetchCountries()
+        .then((countries) => renderCountryList(countries))
+        .catch((error) => console.log(error));
+});
+
+
+function renderCountryList(countries) {
+    const countryListMarkup = countries
+        .map((country) => {
+            return `
+            <svg src="${country.flags.svg}" alt="flag"></svg>
+            ${country.name.official}`;
+        })
+        .join("");
+    countryList.innerHTML = countryListMarkup;
+
+    const countryInfoMarkup = countries
+        .map((country) => {
+            return `
+            <p><b>Capital:</b> ${country.capital}</p>
+            <p><b>Population:</b> ${country.population}</p>
+            <p><b>Languages:</b> ${country.languages}</p>
+            `
+        })
+        .join("");
+    countryInfo.innerHTML = countryInfoMarkup;
+}
+
